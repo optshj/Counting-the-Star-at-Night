ef818714cf6136fa2ea63e8f4f8edafe490332fd
optshj <optshj@gmail.com>
2025-12-20 17:12:32 +0900

    ➕ [Docs] eslint 설정 변경

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,14 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { HomePage } from "../pages/home"
-import { PlacePage } from "../pages/place"
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HomePage } from '../pages/home';
+import { PlacePage } from '../pages/place';
 
 export default function App() {
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/place' element={<PlacePage />} />
-      </Routes>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/place" element={<PlacePage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
-
