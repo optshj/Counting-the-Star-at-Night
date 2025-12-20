31b0ebad66661e68e9757a140270903f0c72a137
optshj <optshj@gmail.com>
2025-12-20 17:16:48 +0900

    🧑‍💻 [Docs] path alias 추가
    [Docs] path alias 추가

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { HomePage } from '../pages/home';
-import { PlacePage } from '../pages/place';
+import { HomePage } from '@/pages/home';
+import { PlacePage } from '@/pages/place';
 
 export default function App() {
     return (
